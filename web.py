1c81654c4a024285192a51f85b40708c6c0cbe28
mole828 <mrx800@163.com>
2023-06-30 15:07:47 +0800

    解决请求格式的问题

--- web.py
+++ web.py
@@ -8,6 +8,7 @@ import io
 from PIL import Image, ImageDraw
 from fastapi import FastAPI, HTTPException, UploadFile, File
 from fastapi.responses import StreamingResponse, JSONResponse
+from pydantic import BaseModel
 
 from Comb import CombineModel
 
@@ -38,66 +39,38 @@ async def detect_one(file:UploadFile=File(...)):
     return StreamingResponse(content=buffer, media_type="image/jpeg")
 
 
+class ColorRequest(BaseModel):
+    image: str
+    onePieceTaskRecordId: str
+    number: str
+    physicId: str
+
+
 @app.post('/ml/color')
-async def classify_color(data: dict):
+async def handle_color_ml(request: ColorRequest):
+    color = 'none'
     try:
-        # Convert base64 image to a PIL Image
-        image_base64 = data['image']
-        print(f"data: {data}")
+        image_base64 = request.image
         image_data = base64.b64decode(image_base64)
         image = Image.open(io.BytesIO(image_data)).convert("RGB")
-        
-        # # Convert image to a NumPy array and normalize the pixel values
-        # image_array = np.array(image) / 255.0
-        
-        # # Predict the class of the image
-        # result = model.predict(np.array([image_array]))
-        # predictions = [
-        #     {"label": dict_labels[str(index)], "prob": float(value)}
-        #     for index, value in enumerate(result[0])
-        # ]
-        # predictions = sorted(predictions, key=lambda x: x['prob'], reverse=True)
-        # item = predictions[0]
-        # is_right = True if item['prob'] >= 0.5 else False
         colors = model.find(image)
         if len(colors) != 1:
-            return {
-                'code': '0',
-                'msg': '请求成功',
-                'data': {
-                    'isRight': True,
-                    'label': 'white'
-                },
-            }
-
-        # Post the result to a specific URL
-        # Make sure to replace 'ssBasicUrl' with the actual URL
-        # ssBasicUrl = "http://example.com"
-        # url = f"{ssBasicUrl}/api/master/ml/logMlResult"
-        # headers = {'Content-Type': 'application/json', 'appid': 'basic'}
-        # data = {
-        #     "image": image_base64,
-        #     "onePieceTaskRecordId": data.onePieceTaskRecordId,
-        #     "number": data.number,
-        #     "physicId": data.physicId,
-        #     "predictions": predictions,
-        #     "closestPrediction": item,
-        #     "isRight": is_right
-        # }
-        # response = requests.post(url, headers=headers, json=data)
-        
-        # Return the response
+            color = 'white'
+        else:
+            color = colors[0]
         return JSONResponse(status_code=200, content={
             "code": "0",
             "msg": "请求成功",
             "data": {
                 "isRight": True,
-                "label": colors[0]
+                "label": color
             }
         })
     except Exception as error:
         print(error)
         raise HTTPException(status_code=500)
+    finally:
+        print(f"physicId: {request.physicId}, number: {request.number}, color: {color}")
 
 if __name__ == '__main__':
     import uvicorn
